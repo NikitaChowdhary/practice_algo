48a3d4a5155a69ad92f406768498747637729ea3
Nikita Chowdhary <nikita.chowdhary@schibsted.com>
2019-02-06 14:46:03 +0000

    Covering cases where input has aaaa, baaa, bcaa

--- src/codeforces/FoxAndNames_510C.java
+++ src/codeforces/FoxAndNames_510C.java
@@ -11,15 +11,16 @@ public class FoxAndNames_510C {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         int count = Integer.parseInt(in.readLine());
 
-        Map<Character, Character> newOrder = getNewWordOrdering(in, count);
+        Map<Character, List<Character>> newOrder = getNewWordOrdering(in, count);
         Map<Character, Character> original= generateOriginalOrder(newOrder);
-        PriorityQueue<Character> queue = getQueue(newOrder, original);
+        Map<Character, Integer> incoming = new HashMap<>();
+        PriorityQueue<Character> queue = getQueue(newOrder, original, incoming);
 
-        getFinalOrdering(newOrder, original, queue);
+        getFinalOrdering(newOrder, original, queue, incoming);
     }
 
-    private static Map<Character, Character> getNewWordOrdering(BufferedReader in, int count) throws IOException {
-        Map<Character, Character> newOrder = new HashMap<>();
+    private static Map<Character, List<Character>> getNewWordOrdering(BufferedReader in, int count) throws IOException {
+        Map<Character, List<Character>> newOrder = new HashMap<>();
         String currentString = in.readLine();
         for (int i = 0; i< count - 1; i++) {
             String nextString = in.readLine();
@@ -28,7 +29,9 @@ public class FoxAndNames_510C {
                 if (currentString.charAt(j) != nextString.charAt(j)) {
                     char start = currentString.charAt(j);
                     char end = nextString.charAt(j);
-                    newOrder.put(start, end);
+                    List<Character> adj = newOrder.getOrDefault(start, new ArrayList<>());
+                    adj.add(end);
+                    newOrder.put(start, adj);
                     break;
                 }
             }
@@ -37,7 +40,11 @@ public class FoxAndNames_510C {
         return newOrder;
     }
 
-    private static void getFinalOrdering(Map<Character, Character> updated, Map<Character, Character> original, PriorityQueue<Character> queue) {
+    private static void getFinalOrdering(
+            Map<Character, List<Character>> updated,
+            Map<Character, Character> original,
+            PriorityQueue<Character> queue,
+            Map<Character, Integer> incoming) {
         List<Character> result = new ArrayList<>();
         while(!queue.isEmpty()) {
             char current = queue.poll();
@@ -48,8 +55,12 @@ public class FoxAndNames_510C {
                 result.add(current);
                 if (original.containsKey(current))
                     queue.add(original.get(current));
-                if (updated.containsKey(current))
-                    queue.add(updated.get(current));
+                for (char adj: updated.getOrDefault(current, new ArrayList<>())) {
+                    int currentIncoming = incoming.get(adj);
+                    incoming.put(adj, currentIncoming--);
+                    if (currentIncoming == 0 && !result.contains(adj))
+                        queue.add(adj);
+                }
             }
         }
         if (result.size() != 26) System.out.println("Impossible");
@@ -60,19 +71,27 @@ public class FoxAndNames_510C {
         }
     }
 
-    private static PriorityQueue<Character> getQueue(Map<Character, Character> updated, Map<Character, Character> original) {
+    private static PriorityQueue<Character> getQueue(Map<Character, List<Character>> updated, Map<Character, Character> original, Map<Character, Integer> incoming) {
         PriorityQueue<Character> queue = new PriorityQueue<>();
         if (!original.isEmpty())
             queue.add(original.keySet().iterator().next());
         for (char c: updated.keySet()) {
-            // Add all which has 0 incoming egde, as there can be only one incoming edge to a node.
-            if (!updated.values().contains(c))
+            if (!incoming.containsKey(c))
+                incoming.put(c, 0);
+            for (char adj: updated.get(c)) {
+                int current = incoming.getOrDefault(adj, 0);
+                incoming.put(adj, current + 1);
+            }
+        }
+
+        for (char c: incoming.keySet()) {
+            if (incoming.get(c) == 0)
                 queue.add(c);
         }
         return queue;
     }
 
-    private static Map<Character, Character> generateOriginalOrder(Map<Character, Character> updatedKeys) {
+    private static Map<Character, Character> generateOriginalOrder(Map<Character, List<Character>> updatedKeys) {
         Map<Character, Character> original = new HashMap<>();
 
         List<Character> originalLexico = new ArrayList<>();
@@ -80,7 +99,8 @@ public class FoxAndNames_510C {
             originalLexico.add((char)(i+97));
 
         originalLexico.removeAll(updatedKeys.keySet());
-        originalLexico.removeAll(updatedKeys.values());
+        for (List<Character> ch: updatedKeys.values())
+            originalLexico.removeAll(ch);
 
         for (int startPos = 0; startPos < originalLexico.size() - 1; startPos++) {
             char start = originalLexico.get(startPos);
