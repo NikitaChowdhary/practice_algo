2a7f9fe3e58b4b1c560c848866c5b546fbd4011d
Nikita Chowdhary <nikita.chowdhary@schibsted.com>
2019-02-06 14:04:55 +0000

    Code clean

--- src/codeforces/FoxAndNames_510C.java
+++ src/codeforces/FoxAndNames_510C.java
@@ -11,79 +11,81 @@ public class FoxAndNames_510C {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         int count = Integer.parseInt(in.readLine());
 
-        Map<Character, Character> newOrder = new HashMap<>();
+        Map<Character, Character> newOrder = getNewWordOrdering(in, count);
+        Map<Character, Character> original= generateOriginalOrder(newOrder);
+        PriorityQueue<Character> queue = getQueue(newOrder, original);
 
+        getFinalOrdering(newOrder, original, queue);
+    }
+
+    private static Map<Character, Character> getNewWordOrdering(BufferedReader in, int count) throws IOException {
+        Map<Character, Character> newOrder = new HashMap<>();
         String currentString = in.readLine();
         for (int i = 0; i< count - 1; i++) {
             String nextString = in.readLine();
-            getWordsOrder(currentString, nextString, newOrder);
+
+            for (int j = 0; j < Math.min(currentString.length(), nextString.length()); j++) {
+                if (currentString.charAt(j) != nextString.charAt(j)) {
+                    char start = currentString.charAt(j);
+                    char end = nextString.charAt(j);
+                    newOrder.put(start, end);
+                    break;
+                }
+            }
             currentString = nextString;
         }
-
-        System.out.println(getFinalOrdering(newOrder));
+        return newOrder;
     }
 
-    private static void getWordsOrder(String currentString, String nextString, Map<Character, Character> newOrder) {
-        for (int j = 0; j < Math.min(currentString.length(), nextString.length()); j++) {
-            if (currentString.charAt(j) != nextString.charAt(j)) {
-                char start = currentString.charAt(j);
-                char end = nextString.charAt(j);
-                newOrder.put(start, end);
-                break;
-            }
-        }
-    }
-
-    private static String getFinalOrdering(Map<Character, Character> updated) {
-        String result = "";
-        Set<Character> newOrdered = new HashSet<>();
-        newOrdered.addAll(updated.keySet());
-        newOrdered.addAll(updated.values());
-        Map<Character, Character> original= generateOriginalOrder(newOrdered);
-
-        PriorityQueue<Character> queue = new PriorityQueue<>();
-        for (char c: updated.keySet()) {
-            if (!updated.values().contains(c))
-                queue.add(c);
-        }
-        if (!original.isEmpty())
-            queue.add(original.keySet().iterator().next());
-
-
-        boolean[] visited = new boolean[26];
+    private static void getFinalOrdering(Map<Character, Character> updated, Map<Character, Character> original, PriorityQueue<Character> queue) {
+        List<Character> result = new ArrayList<>();
         while(!queue.isEmpty()) {
             char current = queue.poll();
-            if (visited[current - 97]) {
-                result = "Impossible";
+            if (result.contains(current)) {
                 break;
-            } else {
-                result += current;
+            }
+            else {
+                result.add(current);
                 if (original.containsKey(current))
                     queue.add(original.get(current));
                 if (updated.containsKey(current))
                     queue.add(updated.get(current));
             }
-            visited[current] = true;
         }
-
-        if (result.length() != 26) result = "Impossible";
-        return result;
+        if (result.size() != 26) System.out.println("Impossible");
+        else {
+            for (char c: result)
+                System.out.print(c);
+            System.out.println();
+        }
     }
 
-    private static Map<Character, Character> generateOriginalOrder(Set<Character> newOrdered) {
+    private static PriorityQueue<Character> getQueue(Map<Character, Character> updated, Map<Character, Character> original) {
+        PriorityQueue<Character> queue = new PriorityQueue<>();
+        if (!original.isEmpty())
+            queue.add(original.keySet().iterator().next());
+        for (char c: updated.keySet()) {
+            // Add all which has 0 incoming egde, as there can be only one incoming edge to a node.
+            if (!updated.values().contains(c))
+                queue.add(c);
+        }
+        return queue;
+    }
+
+    private static Map<Character, Character> generateOriginalOrder(Map<Character, Character> updatedKeys) {
         Map<Character, Character> original = new HashMap<>();
-        int i = 0;
-        while(newOrdered.contains((char)(i+97)))
-            i++;
-        char start = (char)(i+97);
-        while (i++ < 25) {
-            while(newOrdered.contains((char)(i+97)))
-                i++;
-            if (i < 26) {
-                char end = (char) (i + 97);
-                original.put(start, end);
-                start = end;
-            }
+
+        List<Character> originalLexico = new ArrayList<>();
+        for (int i = 0; i< 26; i++)
+            originalLexico.add((char)(i+97));
+
+        originalLexico.removeAll(updatedKeys.keySet());
+        originalLexico.removeAll(updatedKeys.values());
+
+        for (int startPos = 0; startPos < originalLexico.size() - 1; startPos++) {
+            char start = originalLexico.get(startPos);
+            char end = originalLexico.get(startPos + 1);
+            original.put(start, end);
         }
         return original;
     }
